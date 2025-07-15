a635237253dab3f27cee888055ab308ee3bdaa81
Dan Salas <dansalas@mac.com>
2025-07-14 21:46:46 -0700

    Finish Snake Game Part 2: Add food and scoreboard classes

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-import turtle
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
@@ -18,11 +17,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            snake = Turtle("square")
-            snake.color("white")
-            snake.penup()
-            snake.goto(position)
-            self.segments.append(snake)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake = Turtle("square")
+        snake.color("white")
+        snake.penup()
+        snake.goto(position)
+        self.segments.append(snake)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
             for seg_num in range(len(self.segments) - 1, 0, -1):
